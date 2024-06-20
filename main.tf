095c05053ba3166fac9d0e1f442c48fc706a8bbb
amoldevops2024 <learndevopsag@gmail.com>
2024-06-20 11:09:57 +0530

    Terraform single file.

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "<Your Access Key>"
+  secret_key = "<Your Secret Key>"
+}
+
+resource "aws_instance" "web" {
+  ami           = "<Your AMI ID>"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+    
+  }
+
+}
