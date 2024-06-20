7a45c7034b2d587c558a6f34274b20721bf382c7
amoldevops2024 <learndevopsag@gmail.com>
2024-06-20 11:24:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
-  access_key = "<Your Access Key>"
-  secret_key = "<Your Secret Key>"
+  access_key = "AKIA4MTWKTVHI5AUPLPP"
+  secret_key = "OeVrUZNFZ94GVuRfOn7bisRjzxzJSCzOKrwW5buH"
 }
 
 resource "aws_instance" "web" {
-  ami           = "<Your AMI ID>"
+  ami           = "ami-0e1d06225679bc1c5"
   instance_type = "t2.micro"
 
   tags = {
